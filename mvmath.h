19956d20a432288a4a5622059bf4d76965d357d0
Corrie <94648401+CorrieJ98@users.noreply.github.com>
2024-04-25 15:51:00 +0100

    Update mvmath.h
    removed redundancies

--- mvmath.h
+++ mvmath.h
@@ -38,28 +38,7 @@ namespace mv {
 
 	class MV_Math
 	{
-	public:/*
-		void Matrix_MakeIdentity(mat4x4&);
-		mat4x4 Matrix_PointAt(v3f&, v3f&, v3f&);
-		mat4x4 Matrix_QuickInvert(mat4x4&);
-		mat4x4 Matrix_RotAxisX(float);
-		mat4x4 Matrix_RotAxisY(float);
-		mat4x4 Matrix_RotAxisZ(float);
-		mat4x4 Matrix_MakeTranslation(float, float, float);
-		mat4x4 Matrix_MakeProjection(float, float, float, float);
-		mat4x4 Matrix_MultiplyMatrix(mat4x4&, mat4x4&);
-		v3f Matrix_MultiplyVector(mat4x4&, v3f&);
-		v3f Vector_Add(v3f&, v3f&);
-		v3f Vector_Subtract(v3f&, v3f&);
-		v3f Vector_Multiply(v3f&, float);
-		v3f Vector_Divide(v3f&, float);
-		float Vector_DotProduct(v3f&, v3f&);
-		float Vector_Length(v3f&);
-		v3f Vector_Normalise(v3f&);
-		v3f Vector_CrossProduct(v3f&, v3f&);
-		v3f Vector_IntersectPlane(v3f&, v3f&, v3f&, v3f&);
-		int Triangle_ClipAgainstPlane(v3f, v3f, triangle&, triangle&, triangle&);*/
-
+	public:
 		mat4x4 Matrix_MakeIdentity()
 		{
 			mat4x4 id;
